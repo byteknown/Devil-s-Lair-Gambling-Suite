5ea34a1d263d797806403ce4547ffac0975e2572
byteknown <byteknown@gmail.com>
2023-02-23 22:32:09 +0100

    staged 1

--- SamplePlugin/Windows/MainWindow.cs
+++ SamplePlugin/Windows/MainWindow.cs
@@ -11,7 +11,8 @@ public class MainWindow : Window, IDisposable
     private readonly DalamudUtils dutils;
 
     private int player1Bet = 100;
-    private int player2Bet = 0;
+    bool activated = false;
+    float width = 2.0f;
 
     private int lastRoll = 0;
 
@@ -51,69 +52,69 @@ public class MainWindow : Window, IDisposable
 
     public override void Draw()
     {
-        if (ImGui.Button("Random"))
-        {
-            _ = dutils.IngameRandom().ContinueWith(t => lastRoll = t.Result);
-        }
 
-        ImGui.Text("Result = " + lastRoll);
-
-        bool activated = false;
-        float width = 2.0f;
-        /* int switchTabs = 3;
-
-         if (ImGui.Button("Aimbot"))
-             switchTabs = 0;
-         ImGui.SameLine();
-         if (ImGui.Button("Visuals"))
-             switchTabs = 1;
-         ImGui.SameLine();
-         if (ImGui.Button("Misc"))
-             switchTabs = 2;
-
-         switch (switchTabs)
-         {
-             case 0:
-                 //Do stuff for Aim tab, next for vis, and last for misc tab
-                 break;
-             case 1:
-                 break;
-             case 2:
-                 break;
-         }
-         ImGui.Separator();*/
         string host = "Moonhell";
-        int totalBet = player1Bet + player2Bet;
+        string totalBet = "members " + dutils.GetGroupMembersCount();
         ImGui.Text($"This is your main window. You won't need any more window if you plan on using tabs.");
         if (ImGui.Checkbox("Enable", ref activated))
         {
-
+             
         }
-        ImGui.BeginTable("Table1", 3);
-        ImGui.TableSetupColumn("Player", 0, width = 3.0f);
-        ImGui.TableSetupColumn("Bets", 0, width = 2.0f);
-        ImGui.TableSetupColumn("Cards", 0, width = 2.0f);
-        ImGui.TableNextColumn();
-        ImGui.Text(host);
-        ImGui.TableNextColumn();
-        ImGui.Text("total bet");
-        ImGui.TableNextColumn();
-        if (ImGui.Button("This is a button"))
+        ImGui.BeginTabBar("MyTabBar");
+
+        if (ImGui.BeginTabItem("Game"))
         {
-            // Do something when pressed
+            ImGui.BeginTable("Table1", 3);
+            ImGui.TableSetupColumn("Player", 0, width = 2.0f);
+            ImGui.TableSetupColumn("Bets", 0, width = 1.0f);
+            ImGui.TableSetupColumn("Cards", 0, width = 1.0f);
+            ImGui.TableNextColumn();
+            ImGui.Text(host);
+            ImGui.TableNextColumn();
+            ImGui.Text(totalBet);
+            ImGui.TableNextColumn();
+            if (ImGui.Button("This is a button"))
+            {
+                // Do something when pressed
+            }
+            ImGui.TableNextColumn();
+            for (int i = 1; i <= 8; i++)
+            {
+                foreach (string name in dutils.GetGroupMembersNames())
+                {
+                    ImGui.Text("Player : " + name);
+                }
+                ImGui.TableNextColumn();
+                BetsPlacement("player " + player1Bet);
+                ImGui.TableNextColumn();
+                CardsButton();
+                ImGui.TableNextColumn();
+
+            }
+
+            ImGui.EndTable();
+
+            ImGui.EndTabItem();
         }
-        ImGui.TableNextColumn();
-        for (int i = 1; i <= 8; i++)
+
+        if (ImGui.BeginTabItem("Config"))
         {
-            GeneratePlayers("Player " + i);
-            ImGui.TableNextColumn();
-            BetsPlacement("player " + player1Bet);
-            ImGui.TableNextColumn();
-            CardsButton();
-            ImGui.TableNextColumn();
+            // Add your content for Tab 2 here
+            ImGui.Text("This is Tab 2");
 
+            ImGui.EndTabItem();
+        }
+        if (ImGui.BeginTabItem("Help"))
+        {
+            // Add your content for Tab 2 here
+            ImGui.Text("This is Tab 2");
+
+            ImGui.EndTabItem();
         }
 
-        ImGui.EndTable();
+        ImGui.EndTabBar();
+        
+        
+        
     }
 }
